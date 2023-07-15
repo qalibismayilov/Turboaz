d9da5283f1928d3be862e07415d2e49ddf58bbcf
Ismay_be29 <Ismay_be29@STHQ012E-14>
2023-07-15 19:50:56 +0400

    Add project files.

---
+++ WpfApp3/ViewModels/MainViewModel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using WpfApp3.Command;
+using WpfApp3.Views;
+
+namespace WpfApp16.ViewModels
+{
+    public class MainViewModel
+    {
+        public ICommand? NewWindow { get; set; }
+
+        public ObservableCollection<string> MarkaNames { get; set; }
+        public ObservableCollection<string> ModelNames { get; set; }
+        public ObservableCollection<string> YearNames { get; set; }
+        public object CarNames { get; internal set; }
+
+        public MainViewModel()
+        {
+            MarkaNames = new ObservableCollection<string>
+            {
+                "Mercedes",
+                "BMW",
+                "Toyota",
+                "Ford",
+            };
+            ModelNames = new ObservableCollection<string>
+            {
+                "CLS 63",
+                "e60",
+                "land cruiser",
+                "fushion",
+            };
+            YearNames = new ObservableCollection<string>
+            {
+                "2003",
+                "2008",
+                "2015",
+                "2017",
+            };
+
+            NewWindow = new RelayCommand(ExecuteNewWindow, CanExecuteNewWindow);
+        }
+        void ExecuteNewWindow(object? parameter)
+        {
+            AddView addview = new AddView();
+            addview.ShowDialog();
+        }
+        bool CanExecuteNewWindow(object? parameter)
+        {
+            return true;
+        }
+    }
+}
---
+++ WpfApp3/Views/MainView.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WpfApp16.ViewModels;
+
+namespace WpfApp3.Views
+{
+    /// <summary>
+    /// Interaction logic for MainView.xaml
+    /// </summary>
+    public partial class MainView : Window
+    {
+        public MainView()
+        {
+            InitializeComponent();
+            DataContext = new MainViewModel(); 
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainViewModel? viewModel = DataContext as MainViewModel;
+            if (viewModel != null)
+            {
+                int selectedIndex = combo.SelectedIndex;
+
+         
+                marka.Text = viewModel.MarkaNames[selectedIndex];
+                model.Text = viewModel.ModelNames[selectedIndex];
+                year.Text = viewModel.YearNames[selectedIndex];
+            }
+        }
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            MainViewModel? viewModel = DataContext as MainViewModel;
+            if (viewModel != null)
+            {
+                int selectedIndex = combo.SelectedIndex;
+                
+                viewModel.MarkaNames.RemoveAt(selectedIndex);
+                viewModel.ModelNames.RemoveAt(selectedIndex);
+                viewModel.YearNames.RemoveAt(selectedIndex);
+
+                marka.Text = "";
+                model.Text = "";
+                year.Text = "";
+            }
+        }
+    }
+}
